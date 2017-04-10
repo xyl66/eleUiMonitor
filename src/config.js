1b016fad5fbb8a549e6161de5516d8ee96987671
xyl66 <492113976@qq.com>
2017-04-10 17:05:16 +0800

    .

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 /**
  * Created by GUNDAM on 5/4/17.
  */
-export const serverIp='http://10.134.159.93/v1/';
+export const serverIp='http://10.130.2.95/v1/';
 export const serverAdminIp='http://10.134.159.93/';
 export const monitorIp='http://10.134.159.91:8080/';
